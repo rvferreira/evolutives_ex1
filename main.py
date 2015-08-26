0b1eaad5db94c825073f0236e26c5ada6d335f3a
rvferreira <raphael.rvf@gmail.com>
2015-08-26 12:01:49 -0300

    mutation and evolve inserted into main loop

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ import sys
 REPRESENTATION = 'REAL'
 CROSSOVER = 'MEAN'
 MUTATION_RATE = 0.1
-MUTATION_EFFECT = 0.01
+MUTATION_EFFECT_MODE = 'PLUS_MINUS'
+MUTATION_EFFECT_INTENSITY = 0.01
 PARENTS_SELECTION = 'ROULETTE'
-INITIAL_POPULATION_SIZE = 1
+INITIAL_POPULATION_SIZE = 5
 INITIAL_POPULATION_MODE = 'RANDOM'
 LAST_GENERATION = 15
 
 population = []
 fitness = []
+
+def population_evolve(population_vector, population_size, crossover_method):
+	print "evolve"
+
+def population_mutate(population_vector, population_size, mutation_rate, mutation_mode, mutation_intensity):
+	print "mutate"
 
 def population_init(population_vector, fitness_vector, initial_size):
 	for i in range(INITIAL_POPULATION_SIZE):
@@ -18,7 +25,7 @@ def population_init(population_vector, fitness_vector, initial_size):
 		fitness_vector.append(1.0)
 
 def ackley_fitness(population_vector, individual_index):
-	return round(1.0, 4)
+	return round(abs(individual_index - 5), 4)
 
 def test_fitness(population_vector, fitness_vector, fitness_function, population_size):
 	for i in range(population_size):
@@ -26,9 +33,8 @@ def test_fitness(population_vector, fitness_vector, fitness_function, population
 		if fitness_vector[i] == 0.0:
 			return i
 
+	print fitness
 	return -1
-
-	
 
 def main():
 	population_init(population, fitness, INITIAL_POPULATION_SIZE)
@@ -42,8 +48,12 @@ def main():
 			print population[zero_fitness_element]
 			break
 
+		population_evolve(population, INITIAL_POPULATION_SIZE, CROSSOVER)
+		population_mutate(population, INITIAL_POPULATION_SIZE, MUTATION_RATE, MUTATION_EFFECT_MODE, MUTATION_EFFECT_INTENSITY)
 
 		print '\n'
+
+
 
 	if zero_fitness_element == -1:
 		best_fitness_element = fitness.index(min(fitness))
