f931fdd167396d979de5d954eae9d2421cfeea75
ValeskaParoni <valeskaparoni@gmail.com>
2015-08-26 12:18:22 -0300

    Merging Files

--- ackley.py
+++ ackley.py
@@ -1,16 +1,21 @@
 from random import seed, randrange
 from math import cos, exp, sqrt, pi
 
-population = []
 MIN_X_RANGE = 0.0
 MAX_X_RANGE = 1.0
-INDIVIDUAL_DIMENSIONS_COUNT = 2
-POPULATION_SIZE = 100
 C3 = 2*pi
 C2 = 0.2
 C1 = 20
 
-def population_init(population_size, population_vector, individual_dimensions_count):
+def population_evolve(population_vector, population_size, crossover_method):
+	print "evolve"
+
+
+def population_mutate(population_vector, population_size, mutation_rate, mutation_mode, mutation_intensity):
+	print "mutate"
+
+
+def population_init(population_vector, fitness_vector, population_size, individual_dimensions_count):
 	seed()
 
 	for i in range(population_size):
@@ -20,21 +25,18 @@ def population_init(population_size, population_vector, individual_dimensions_co
 			individual_dimensions.append(randrange(MIN_X_RANGE*100, MAX_X_RANGE*100) / 100.0)
 
 		population_vector.append(individual_dimensions)
-		print population_vector[-1]
-	fitness = []
-	ackley_fitness(population_vector, individual_dimensions_count, population_size, fitness)
+		fitness_vector.append(100.0)
 
 
-
-def ackley_fitness(population_vector, individual_dimensions_count, index):
+def ackley_fitness(population_vector, index, individual_dimensions_count):
 
 	s1 = 0
 	s2 = 0
 	for j in range (individual_dimensions_count):
-		s1 = s1 + (population_vector[i][j] * population_vector[i][j])
-		s2 = s2 + (cos(C3*population_vector[i][j]))
+		s1 = s1 + (population_vector[index][j] * population_vector[index][j])
+		s2 = s2 + (cos(C3*population_vector[index][j]))
 
-	return (-C1*exp(-C2*sqrt((1.0/individual_dimensions_count)*s1)-exp((1.0/individual_dimensions_count)*s2))+C1+1)
+	return round(-C1*exp(-C2*sqrt((1.0/individual_dimensions_count)*s1)-exp((1.0/individual_dimensions_count)*s2))+C1+1, 6)
 
 
 if __name__ == '__main__': population_init(3, population, 2) #ackley_fitness(population, 2, 100, fitness)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from ackley import population_evolve, population_mutate, population_init, ackley_fitness
 
 REPRESENTATION = 'REAL'
 CROSSOVER = 'MEAN'
@@ -8,41 +9,29 @@ MUTATION_EFFECT_INTENSITY = 0.01
 PARENTS_SELECTION = 'ROULETTE'
 INITIAL_POPULATION_SIZE = 5
 INITIAL_POPULATION_MODE = 'RANDOM'
+INDIVIDUAL_DIMENSIONS_COUNT = 2
 LAST_GENERATION = 15
 
 population = []
 fitness = []
 
-def population_evolve(population_vector, population_size, crossover_method):
-	print "evolve"
-
-def population_mutate(population_vector, population_size, mutation_rate, mutation_mode, mutation_intensity):
-	print "mutate"
-
-def population_init(population_vector, fitness_vector, initial_size):
-	for i in range(INITIAL_POPULATION_SIZE):
-		population_vector.append([1.0, 1.1, 1.2])
-		fitness_vector.append(1.0)
-
-def ackley_fitness(population_vector, individual_index):
-	return round(abs(individual_index - 5), 4)
-
-def test_fitness(population_vector, fitness_vector, fitness_function, population_size):
+def test_fitness(population_vector, fitness_vector, fitness_function, population_size, individual_dimensions):
 	for i in range(population_size):
-		fitness_vector[i] = fitness_function(population_vector, i)
+		fitness_vector[i] = fitness_function(population_vector, i, individual_dimensions)
 		if fitness_vector[i] == 0.0:
 			return i
 
 	print fitness
 	return -1
 
+
 def main():
-	population_init(population, fitness, INITIAL_POPULATION_SIZE)
+	population_init(population, fitness, INITIAL_POPULATION_SIZE, INDIVIDUAL_DIMENSIONS_COUNT)
 	
 	for i in range(LAST_GENERATION):
 
 		print 'GENERATION:', i
-		zero_fitness_element = test_fitness(population, fitness, ackley_fitness, INITIAL_POPULATION_SIZE)
+		zero_fitness_element = test_fitness(population, fitness, ackley_fitness, INITIAL_POPULATION_SIZE, INDIVIDUAL_DIMENSIONS_COUNT)
 		if zero_fitness_element != -1:
 			print "\nOptimal fitness was found at element", zero_fitness_element
 			print population[zero_fitness_element]
