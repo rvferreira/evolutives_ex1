4cf1113c4e94a4fe4162c558c995083708339562
rvferreira <raphael.rvf@gmail.com>
2015-08-26 14:27:28 -0300

    population evolving

--- ackley.py
+++ ackley.py
@@ -1,5 +1,5 @@
 from random import seed, randrange
-from math import cos, exp, sqrt, pi
+from math import cos, exp, sqrt, pi, fsum
 
 MIN_X_RANGE = 0.0
 MAX_X_RANGE = 1.0
@@ -7,8 +7,42 @@ C3 = 2*pi
 C2 = 0.2
 C1 = 20
 
-def population_evolve(population_vector, population_size, crossover_method):
-	print "evolve"
+def parents_select(population_vector, fitness_vector, population_size):
+
+	#roll that prioritizes best parents
+	first_parent_roll = 0.8 ** (randrange(0, 100)/(-5.077) - 1) - 1.2
+	second_parent_roll = 0.8 ** (randrange(0, 100)/(-5.077) - 1) - 1.2
+
+	#normalizing for population size
+	first_parent_roll = int((first_parent_roll / 100.0) * population_size)
+	second_parent_roll = int((second_parent_roll / 100.0) * population_size)
+
+	ordered_fitness_vector = sorted(fitness_vector)
+
+	first_parent = population_vector[fitness_vector.index(ordered_fitness_vector[first_parent_roll])]
+	second_parent = population_vector[fitness_vector.index(ordered_fitness_vector[second_parent_roll])]
+
+	return [first_parent, second_parent]
+
+
+def crossover(parents_vector, pc, crossover_method):
+	parents_merge_vector = []
+	if crossover_method == 'MEAN':
+		for i in range(len(parents_vector[0])):
+			parents_merge_vector.append(round(fsum([parents_vector[0][i], parents_vector[1][i]])/len(parents_vector), 4))
+
+	child = parents_vector[0][0:pc] + parents_merge_vector[pc:len(parents_merge_vector)]
+	return child
+
+
+def population_evolve(population_vector, fitness_vector, population_size, crossover_method):
+	new_generation = []
+
+	for i in range(population_size):
+		selected_parents = parents_select(population_vector, fitness_vector, population_size)
+		new_generation.append(crossover(selected_parents, randrange(0,len(population_vector[0])), crossover_method))
+
+	return new_generation
 
 
 def population_mutate(population_vector, population_size, mutation_rate, mutation_mode, mutation_intensity):
--- main.py
+++ main.py
@@ -7,13 +7,10 @@ MUTATION_RATE = 0.1
 MUTATION_EFFECT_MODE = 'PLUS_MINUS'
 MUTATION_EFFECT_INTENSITY = 0.01
 PARENTS_SELECTION = 'ROULETTE'
-INITIAL_POPULATION_SIZE = 5
+INITIAL_POPULATION_SIZE = 50
 INITIAL_POPULATION_MODE = 'RANDOM'
-INDIVIDUAL_DIMENSIONS_COUNT = 2
-LAST_GENERATION = 15
-
-population = []
-fitness = []
+INDIVIDUAL_DIMENSIONS_COUNT = 4
+LAST_GENERATION = 10
 
 def test_fitness(population_vector, fitness_vector, fitness_function, population_size, individual_dimensions):
 	for i in range(population_size):
@@ -21,11 +18,13 @@ def test_fitness(population_vector, fitness_vector, fitness_function, population
 		if fitness_vector[i] == 0.0:
 			return i
 
-	print fitness
 	return -1
 
 
 def main():
+	population = []
+	fitness = []
+
 	population_init(population, fitness, INITIAL_POPULATION_SIZE, INDIVIDUAL_DIMENSIONS_COUNT)
 	
 	for i in range(LAST_GENERATION):
@@ -37,9 +36,12 @@ def main():
 			print population[zero_fitness_element]
 			break
 
-		population_evolve(population, INITIAL_POPULATION_SIZE, CROSSOVER)
+		population = population_evolve(population, fitness, INITIAL_POPULATION_SIZE, CROSSOVER)
 		population_mutate(population, INITIAL_POPULATION_SIZE, MUTATION_RATE, MUTATION_EFFECT_MODE, MUTATION_EFFECT_INTENSITY)
 
+		best_fitness_element = fitness.index(min(fitness))
+		print "Best fitness of this generation was found to be", fitness[best_fitness_element], "at the element", best_fitness_element
+		print population[best_fitness_element]
 		print '\n'
 
 
